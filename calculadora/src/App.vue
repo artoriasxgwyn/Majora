dcd913e9889a10eafc6a43b7b7743055fb624ac3
link <johannsilvamendez.gmail.com>
2025-07-23 10:31:58 -0500

    estoy jodido

---
+++ calculadora/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from 'vue';
+let numero= ref(0)
+setInterval(()=>{
+numero.value++
+},1000)
+</script>
+
+<template>
+  <h1>{{numero}}</h1>
+</template>
+
+<style>
+</style>
