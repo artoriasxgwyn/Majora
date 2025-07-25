c4edd6579a1d55257246edef6a0f8ae833a25f96
link <johannsilvamendez@gmail.com>
2025-07-25 17:37:58 -0500

    data vue info

--- calculadora/src/App.vue
+++ calculadora/src/App.vue
@@ -1,29 +1,11 @@
 <script setup>
-import { ref } from "vue";
-import boton from "./components/button.vue";
-let numero = ref(0);
-let condition = false;
-let entradaTexto="pagame boludo"
 </script>
 
 <template>
   <div>
-    <boton ContentButton="hola mami"></boton>
-    <h1>{{ numero }}</h1>
-  <h1 ></h1>
-    <input type="text" placeholder="hola bro" :value="entradaTexto">
-    <input type="text" placeholder="hola bro" v-model="hola">
-    <h1 v-bind:class="condition ? 'color' : 'color2'">por que debo intentarlo?</h1>
-    <boton ContentButton="jijija"></boton>
+   
   </div>
 </template>
 
 <style scoped>
-.color {
-  color: green;
-}
-
-.color2 {
-  color: red;
-}
 </style>
---
+++ calculadora/src/components/exercise.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from "vue";
+import boton from "./components/button.vue";
+const movies = ["hambre", "muerte", "luz", "penumbra", "odio"];
+let numero = ref(0);
+let condition = true;
+let condition2 = true;
+let entradaTexto = "pagame boludo";
+let name = ref("hotero");
+const conditionp = false;
+const conditionq = false;
+</script>
+
+<template>
+  <div>
+    <boton ContentButton="hola mami"></boton>
+    <h1>{{ numero }}</h1>
+    <h1></h1>
+    <input type="text" placeholder="hola bro" v-bind:value="entradaTexto" />
+    <input type="text" placeholder="hola bro" v-model="name" />
+    <h1 v-bind:class="condition ? 'color' : 'color2'">{{name}}</h1>
+    <h1 v-bind:class="{color2:condition2,color1:condition}">{{name}}</h1>
+    <boton ContentButton="jijija"></boton>
+    <div>
+      <h1>Directiva v-if</h1>
+      <h2 v-if="conditionp">hola</h2>
+      <h3 v-else-if="conditionq">tengo hambre</h3>
+      <h4 v-else>sapo</h4>
+    </div>
+    <h1>Renderizar listas</h1>
+    <div>
+      <h1 v-for="(movie, indexMovie) in movies" :key="movie">
+        {{movie}}
+        {{indexMovie}}
+      </h1>
+    </div>
+     <div>
+      <h1>Renderizar objetos</h1>
+      <h1 v-for="(animal, indexAnimal) in animals" :key="animal">
+        {{animal.name}}
+        {{indexAnimal}}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.color {
+  color: green;
+  text-align: center;
+}
+
+.color1 {
+  color: pink;
+}
+
+.color2 {
+  border: 1px solid green;
+}
+</style>
