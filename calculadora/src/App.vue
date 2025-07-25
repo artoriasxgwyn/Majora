429e9069c0d858f714b8dc591eae56ec47e49fa3
link <johannsilvamendez.gmail.com>
2025-07-25 10:55:20 -0500

    this is  my study of vue

--- calculadora/src/App.vue
+++ calculadora/src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
-import {ref} from 'vue';
-let numero= ref(0)
-setInterval(()=>{
-numero.value++
-},1000)
+import { ref } from "vue";
+import boton from "./components/button.vue";
+let numero = ref(0);
+let condition = false;
+let entradaTexto="pagame boludo"
 </script>
 
 <template>
-  <h1>{{numero}}</h1>
+  <div>
+    <boton ContentButton="hola mami"></boton>
+    <h1>{{ numero }}</h1>
+  <h1 ></h1>
+    <input type="text" placeholder="hola bro" :value="entradaTexto">
+    <input type="text" placeholder="hola bro" v-model="hola">
+    <h1 v-bind:class="condition ? 'color' : 'color2'">por que debo intentarlo?</h1>
+    <boton ContentButton="jijija"></boton>
+  </div>
 </template>
 
 <style>
+.color {
+  color: green;
+}
+
+.color2 {
+  color: red;
+}
 </style>
---
+++ calculadora/src/components/button.vue
@@ -0,0 +1,20 @@
+<script setup>
+let props = defineProps({
+  ContentButton: {
+    require: true,
+    type: String
+  },
+});
+</script>
+
+<template>
+  <div>
+    <button>{{ props.ContentButton }}</button>
+  </div>
+</template>
+
+<style>
+button {
+  padding: 17px;
+}
+</style>
