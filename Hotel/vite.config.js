0f15ddd429c333fb347833a1585187db83dd87b9
Link <joahnnsilvamendez@gmail.com>
2025-08-26 15:01:04 -0500

    proyecto vue desplegado

--- Hotel/vite.config.js
+++ Hotel/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url'
 import { quasar,transformAssetUrls } from '@quasar/vite-plugin'
 export default defineConfig({
+  base:'./',
   plugins: [vue({
     template:{transformAssetUrls}
   }),
